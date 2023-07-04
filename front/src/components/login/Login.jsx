9aae6464fcb5d93dc78d6f05940b35de96670fdf
hooman <hooman.honarvar2770@gmail.com>
2023-07-04 15:00:54 +0330

    sign_in and sign_up page

---
+++ front/src/components/login/Login.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from "react";
+import { TextField,Typography ,Button, backdropClasses} from "@mui/material";
+import Image from './1.png'
+import './login.css'
+const Login=()=>{
+    const [inputs, setInputs] = useState({
+        username:"",
+        pass:""
+    });
+    return(
+
+        <div className="maincontainer">
+            <div className="Frame2">
+                <div className="Frame6">
+                    <img src={Image} alt="logo" />
+                </div>
+                <div className="Frame7">
+                    <Typography variant="">
+                        USERNAME:
+                    </Typography>
+                </div>
+                <div className="Frame3">
+                    <TextField variant="outlined" label="USERNAME" className="field" > 
+
+                    </TextField>
+                </div>
+                <div className="Frame4">
+                    <Typography>
+                        PASSWORD:
+                    </Typography>
+                </div>
+                <div className="Frame5">
+                    <TextField variant="outlined" label="PASSWORD" type="password" className="field">
+
+                    </TextField>
+                </div> 
+                <div className="Frame1">
+                    <button className="button" type="submit">
+                        Submit
+                    </button>   
+                </div>           
+            </div> 
+        </div>
+    )
+}
+
+export default Login;
