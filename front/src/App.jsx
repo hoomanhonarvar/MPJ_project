02099824f0757f898b816c351eb2244cbfd9a1c1
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2023-07-10 11:03:54 +0330

    adding yarn

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,10 @@
 import Login from "./components/login/Login";
-
+import P2P from "./components/P2P/P2P"
 function App() {
   return (
     <div className="App">
       
-      <Login/>
+      
 
     </div>
   );
---
+++ front/src/components/P2P/P2P.jsx
@@ -0,0 +1,18 @@
+import react from 'React'
+
+import './P2P.css'
+const P2P=()=>{
+    return(
+
+        <div>
+
+            
+        </div>
+
+    );
+
+
+}
+
+
+export default P2P;
